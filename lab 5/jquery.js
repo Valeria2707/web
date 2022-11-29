b26d098987eeb244bc960e2db41d553417f2d95f
Valeria Zavadetska <Valeria.zavadetska@gmail.com>
2022-11-29 18:52:02 +0200

    fixed

--- lab 5/jquery.js
+++ lab 5/jquery.js
@@ -16,16 +16,6 @@ $( function() {
 });
 
 Share = {
-    /**
-     * ПОКАЗАТЬ ПОЛЬЗОВАТЕЛЮ ДИАЛОГ ШАРИНГА В СООВЕТСТВИИ С ОПЦИЯМИ
-     * МЕТОД ДЛЯ ИСПОЛЬЗОВАНИЯ В INLINE-JS В ССЫЛКАХ
-     * ПРИ БЛОКИРОВКЕ ВСПЛЫВАЮЩЕГО ОКНА ПОДСТАВИТ НУЖНЫЙ АДРЕС И ПОЛЗВОЛИТ БРАУЗЕРУ ПЕРЕЙТИ ПО НЕМУ
-     *
-     * @EXAMPLE <A HREF="" ONCLICK="RETURN SHARE.GO(THIS)">LIKE+</A>
-     *
-     * @PARAM OBJECT _ELEMENT - ЭЛЕМЕНТ DOM, ДЛЯ КОТОРОГО
-     * @PARAM OBJECT _OPTIONS - ОПЦИИ, ВСЕ НЕОБЯЗАТЕЛЬНЫ
-     */
     go: function(_element, _options) {
         var
             self = Share,
@@ -170,51 +160,3 @@ $(document).on('click', '.social_share', function(){
     Share.go(this);
 });
 
-$(document).ready(function () {
-    function currentTime() {
-      var date = new Date();
-      var day = date.getDay();
-      var hour = date.getHours();
-      var min = date.getMinutes();
-      var sec = date.getSeconds();
-      var month = date.getMonth();
-      var currDate = date.getDate();
-      var year = date.getFullYear();
-      var monthName = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-      var showDay = $('.dayDiv span')
-      var midDay= "AM"
-      midDay = (hour>=12)? "PM":"AM";
-      hour = (hour==0)?12:((hour<12)? hour:(hour-12));
-      hour = updateTime(hour);
-      min = updateTime(min);
-      sec = updateTime(sec);
-      currDate= updateTime(currDate);
-      $("#time").html(`${hour}:${min}`);
-      $("#sec").html(`${sec}`);
-      $("#med").html(`${midDay}`);
-      $("#full-date").html(`${monthName[month]} ${currDate} ${year}`);
-      showDay.eq(day).css('opacity','1')
-    }
-    updateTime = function(x){
-      if(x<10){ 
-        return "0"+x
-      }
-      else{
-        return x;
-      }
-    }
-    setInterval(currentTime,1000);
-  });
